b71cae55b89c96d38dd69b3814136f4bbe1e7d03
Manoj-14 <manumadhu1425@gmail.com>
2025-09-15 13:41:59 +0530

    feat: Dockerfile for login service is added (fixes #5)

--- login-service/src/main/java/com/microservices/login/security/JwtAuthenticationFilter.java
+++ login-service/src/main/java/com/microservices/login/security/JwtAuthenticationFilter.java
@@ -8,11 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
